d5fe811b1c1d093dff20efb8c6b2282f7f1e8e10
RAHUL SHARMA <73328975+RAHULSHARMA63@users.noreply.github.com>
2021-08-24 10:34:35 +0530

    To Display multiplication table up to a given range.

---
+++ Display multiplication table up to a given range.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, range;
+    cout << "Enter an integer: ";
+    cin >> n;
+    cout << "Enter range: ";
+    cin >> range;
+    
+    for (int i = 1; i <= range; ++i) 
+	{
+        cout << n << " * " << i << " = " << n * i << "\n";
+    }    
+    return 0;
+}
